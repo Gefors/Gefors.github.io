1dffce0415be76a880a3c52e5a91ee67323b17ba
Gefors <112483367+Gefors@users.noreply.github.com>
2025-08-18 12:56:00 +0200

    Update Header component and build assets
    Renamed and updated the main JS asset in dist/assets, updated dist/index.html to reference the new asset, and made changes to src/components/MainPageComponents/Header.tsx. These changes likely reflect new or improved functionality in the Header component and a corresponding rebuild of frontend assets.

--- src/components/MainPageComponents/Header.tsx
+++ src/components/MainPageComponents/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
       </div>
       <div className="absolute inset-0 flex items-center justify-center">
         <h1 className="text-center text-3xl tracking-wider font-bold font-mono text-white">
-          Welcome to my website <br />
+          Welcome to my website! <br />
           <span className="text-2xl">By Teo Gefors</span>
         </h1>
       </div>
